216abdcd2cf7814c7d0c73da33400a4a5c4d8a56
danielsitepu36 <danielsitepu96@gmail.com>
2020-09-28 22:06:25 +0700

    v1React Release

---
+++ precache-manifest.fcafb6d1665e55705112005b4abb9593.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c2345740b9778b28aba49d482b7d48d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5419193d9fce16ac3d2a",
+    "url": "/static/css/main.aa66cd62.chunk.css"
+  },
+  {
+    "revision": "3ee1a6889f6deb7e9220",
+    "url": "/static/js/2.38e58dd3.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.38e58dd3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5419193d9fce16ac3d2a",
+    "url": "/static/js/main.62ae038c.chunk.js"
+  },
+  {
+    "revision": "6f98b807129621d21225",
+    "url": "/static/js/runtime-main.d98203e2.js"
+  }
+]);
